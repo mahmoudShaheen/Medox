22035e76bad7ff2dcd048a45ebf2851f63594280
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-10 09:48:09 +0200

    FCM Functions added in Notifications Class

--- app/src/main/java/com/slothnull/android/medox/Notifications.java
+++ app/src/main/java/com/slothnull/android/medox/Notifications.java
@@ -2,12 +2,60 @@ package com.slothnull.android.medox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Notifications extends Activity {
+
+    private static final String TAG = "Notifications";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+    }
+
+    public void subscribe(View view){
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        // [END subscribe_topics]
+
+        // Log and toast
+        String msg = "Subscribed to news topic";
+        Log.d(TAG, msg);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+
+    }
+    public void logToken(View view){
+        // Get token
+        String token = FirebaseInstanceId.getInstance().getToken();
+
+        // Log and toast
+        String msg = "InstanceID Token: " + token;
+        Log.d(TAG, msg);
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+
     }
 }
