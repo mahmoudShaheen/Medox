b003a44b05f70f11bc9a8415bdfbf2981e7cf114
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-10 09:21:40 +0200

    nothing

--- app/src/main/java/com/slothnull/android/medox/Splash.java
+++ app/src/main/java/com/slothnull/android/medox/Splash.java
@@ -10,6 +10,7 @@ public class Splash extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
