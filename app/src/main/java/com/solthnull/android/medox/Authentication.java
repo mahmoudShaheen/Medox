78840d66f1eeb87dca4d6efffb049ecd4b26a444
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-10 06:46:05 +0200

    Authentication empty activity

---
+++ app/src/main/java/com/solthnull/android/medox/Authentication.java
@@ -0,0 +1,13 @@
+package com.solthnull.android.medox;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Authentication extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
