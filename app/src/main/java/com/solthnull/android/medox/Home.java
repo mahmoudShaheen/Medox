af46e5fece8f49c7d50a54b366d3182f6a3c9940
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-10 05:44:13 +0200

    buttons added to home activity
    buttons added to home activity for testing other activities

--- app/src/main/java/com/solthnull/android/medox/Home.java
+++ app/src/main/java/com/solthnull/android/medox/Home.java
@@ -1,7 +1,9 @@
 package com.solthnull.android.medox;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends Activity {
 
@@ -9,5 +11,42 @@ public class Home extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void auth(View view){
+        Intent intent = new Intent(this, Authentication.class);
+        startActivity(intent);
+    }
+    public void notification(View view){
+        Intent intent = new Intent(this, Notifications.class);
+        startActivity(intent);
+    }
+    public void welcome(View view){
+        Intent intent = new Intent(this, Welcome.class);
+        startActivity(intent);
+    }
+    public void location(View view){
+        Intent intent = new Intent(this, Location.class);
+        startActivity(intent);
+    }
+    public void indicators(View view){
+        Intent intent = new Intent(this, Indicators.class);
+        startActivity(intent);
+    }
+    public void schedule(View view){
+        Intent intent = new Intent(this, Schedule.class);
+        startActivity(intent);
+    }
+    public void warehouse(View view){
+        Intent intent = new Intent(this, Warehouse.class);
+        startActivity(intent);
+    }
+    public void settings(View view){
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+    public void emergency(View view){
+        Intent intent = new Intent(this, Emergency.class);
+        startActivity(intent);
     }
 }
