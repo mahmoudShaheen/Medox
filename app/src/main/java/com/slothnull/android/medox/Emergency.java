db333e0476b30a492a5b4fc51bb3428729f26b77
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-12 05:07:37 +0200

    Emergency class code

--- app/src/main/java/com/slothnull/android/medox/Emergency.java
+++ app/src/main/java/com/slothnull/android/medox/Emergency.java
@@ -1,13 +1,62 @@
 package com.slothnull.android.medox;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+//TODO: add other commands
 public class Emergency extends Activity {
+
+    private AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+    private boolean check = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
+
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Yes button clicked
+                        check = true;
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+        builder.setMessage("Are you sure?").setPositiveButton("Yes", dialogClickListener)
+                .setNegativeButton("No", dialogClickListener);
+
+    }
+
+    public void cmdDo(View view){
+        Button button = (Button) view;
+        String cmd = (String) button.getText();
+        builder.show();
+        if (check) {
+            sendCommand(cmd);
+        }
+        check = false;
+    }
+
+    public void sendCommand(String cmd){
+        //send notification to database to access it later in Notification Activity
+        AbstractCommand command = new AbstractCommand(cmd);
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("users").child(UID).child("command").push();
+        mDatabase.setValue(command);
     }
 }
