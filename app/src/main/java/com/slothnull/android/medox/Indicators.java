a4343be412644f6fd4974a8db5a0150e9238389b
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-12 07:55:45 +0200

    unused imports removed from Indecators class

--- app/src/main/java/com/slothnull/android/medox/Indicators.java
+++ app/src/main/java/com/slothnull/android/medox/Indicators.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
