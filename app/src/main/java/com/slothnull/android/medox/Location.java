e904f86520a2ba7eca9127b4f30ac6ef5795eb22
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-03-13 02:09:15 +0200

    Location DONE !!!!! :+1:

--- app/src/main/java/com/slothnull/android/medox/Location.java
+++ app/src/main/java/com/slothnull/android/medox/Location.java
@@ -2,6 +2,8 @@ package com.slothnull.android.medox;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +11,19 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Location extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final String TAG = "Location";
+    public double longitude = 0;
+    public double latitude = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +49,55 @@ public class Location extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener dataListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                AbstractData data = dataSnapshot.getValue(AbstractData.class);
+                if (data != null) {
+                    longitude = Double.parseDouble(data.longitude);
+                    latitude = Double.parseDouble(data.latitude);
+                    updateMarker();
+                    // ...
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        mDatabase.child("users").child(UID).child("data")
+                .addValueEventListener(dataListener);
+        //add to list here
+        updateMarker(); //initial place
+    }
+
+    //TODO: add marker for mobile Location and distance
+    //TODO: move refresh button it hides google maps app shortcut
+    //TODO: auto Refresh
+    public void updateMarker(){
+        // clear map Add a marker for watch and move the camera
+        mMap.clear();
+        LatLng watch = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(watch).title("Watch"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(watch, 12));
+    }
+
+    public void refreshData(View view){ //sendRefreshRequest
+        String cmd = "data";
+        //send command to database for raspberry to fetch
+        AbstractCommand command = new AbstractCommand(cmd);
+        //TODO: add if (UID != null) to all classes
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference()
+                .child("users").child(UID).child("watchCommand").push();
+        mDatabase.setValue(command);
     }
 }
