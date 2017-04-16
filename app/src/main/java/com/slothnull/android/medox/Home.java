5b2f964e8b8f2e9b03bcba1e51828d4dec6497ac
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-17 01:54:22 +0200

    options menu done

--- app/src/main/java/com/slothnull/android/medox/Home.java
+++ app/src/main/java/com/slothnull/android/medox/Home.java
@@ -8,9 +8,13 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.slothnull.android.medox.fragment.EmergencyFragment;
 import com.slothnull.android.medox.fragment.IndicatorsFragment;
 import com.slothnull.android.medox.fragment.LocationFragment;
@@ -89,43 +93,37 @@ public class Home extends AppCompatActivity {
         mainTab = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(6);
         mainTab.setBackgroundResource(R.drawable.location);
     }
-/*
-    public void auth(View view){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.signout) {
+            signout();
+            finish();
+            return true;
+        }if (i == R.id.settings) {
+            settings();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    public void signout(){
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
         Intent intent = new Intent(this, Authentication.class);
         startActivity(intent);
     }
-    public void notification(View view){
-        Intent intent = new Intent(this, Notifications.class);
-        startActivity(intent);
-    }
-    public void welcome(View view){
-        Intent intent = new Intent(this, Welcome.class);
-        startActivity(intent);
-    }
-    public void location(View view){
-        Intent intent = new Intent(this, Location.class);
-        startActivity(intent);
-    }
-    public void indicators(View view){
-        Intent intent = new Intent(this, Indicators.class);
-        startActivity(intent);
-    }
-    public void schedule(View view){
-        Intent intent = new Intent(this, Schedule.class);
-        startActivity(intent);
-    }
-    public void warehouse(View view){
-        Intent intent = new Intent(this, Warehouse.class);
-        startActivity(intent);
-    }
-    public void settings(View view){
+    public void settings(){
         Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
-    public void emergency(View view){
-        Intent intent = new Intent(this, Emergency.class);
-        startActivity(intent);
-    }*/
 
     //ProgressDialog
 /*
