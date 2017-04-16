e0212ec7d337123e7367185eca8aaacb6fb9b925
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-16 21:44:43 +0200

    Schedule Fragment

--- app/src/main/java/com/slothnull/android/medox/fragment/ScheduleFragment.java
+++ app/src/main/java/com/slothnull/android/medox/fragment/ScheduleFragment.java
@@ -2,14 +2,37 @@ package com.slothnull.android.medox.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.slothnull.android.medox.Abstract.AbstractSchedule;
 import com.slothnull.android.medox.R;
 
-public class ScheduleFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ScheduleFragment extends Fragment implements View.OnClickListener {
+
+
+    private static final String TAG = "Schedule";
+    public ListView scheduleList;
+    public ArrayAdapter arrayAdapter;
+    public List<String> arrayList = new ArrayList<>();
+    public List<String> keyList = new ArrayList<>();
+
+    View view;
     public ScheduleFragment() {
         // Required empty public constructor
     }
@@ -18,7 +41,105 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        view = inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        scheduleList = (ListView)view.findViewById(R.id.listSchedule);
+        refreshList();
+        scheduleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                deleteEntry(keyList.get(position));
+            }
+        });
+        return view;
+    }
+
+
+    private void refreshList(){
+        //TODO: error here if called more than once the list display any item twice
+        //TODO: new activity for adding new time, this will also solve the problem
+        /*
+        //arrayList = new ArrayList<>();
+        //keyList = new ArrayList<>();
+        arrayList.clear();
+        Log.d(TAG, Integer.toString( arrayList.size()));
+
+        keyList.clear();
+        Log.d(TAG, Integer.toString( keyList.size()));
+
+
+        if (arrayAdapter != null) {
+            arrayAdapter.clear();
+            scheduleList.setAdapter(arrayAdapter);
+        }
+        */
+
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener timetableListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (DataSnapshot child: dataSnapshot.getChildren()){
+                    AbstractSchedule timetable = child.getValue(AbstractSchedule.class);
+                    if (timetable != null) {
+                        arrayList.add( timetable.time  + "\n"  + timetable.billArray );
+                        keyList.add(child.getKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        mDatabase.child("users").child(UID).child("timetable")
+                .addValueEventListener(timetableListener);
+        //add to list here
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        scheduleList.setAdapter(arrayAdapter);
+    }
+
+    public void addEntry(){
+        TextView time =  (TextView) view.findViewById(R.id.textTime);
+        TextView billArray =  (TextView) view.findViewById(R.id.textBillArray);
+        //add new schedule
+        AbstractSchedule timetable = new AbstractSchedule(time.getText().toString(), billArray.getText().toString());
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference()
+                .child("users").child(UID).child("timetable").push();
+        mDatabase.setValue(timetable);
+        //refreshList();
+    }
+
+    public void deleteEntry(String key){
+        //TODO error after deleting from adapter the error message is mentioned after the function
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference()
+                .child("users").child(UID).child("timetable");
+        mDatabase.child(key).setValue(null);
+        //refreshList();
+    }
+    /*
+    ava.lang.IllegalStateException: The content of the adapter has changed but ListView did not
+    receive a notification. Make sure the content of your adapter is not modified from a background
+    thread, but only from the UI thread. Make sure your adapter calls notifyDataSetChanged() when
+    its content changes. [in ListView(2131427456, class android.widget.ListView)
+    with Adapter(class android.widget.ArrayAdapter)]
+     */
+
+    @Override
+    public void onClick(View view) {
+        //do what you want to do when button is clicked
+        switch (view.getId()) {
+            case R.id.addEntry:
+                addEntry();
+                break;
+        }
     }
 
 }
