226dce6514d2eca9b9fab63126988865db73cb2b
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-17 01:01:32 +0200

    Some GUI Update
    theme, icon, colors

--- app/src/main/java/com/slothnull/android/medox/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/slothnull/android/medox/fcm/MyFirebaseMessagingService.java
@@ -126,7 +126,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_notification_round)
+                .setSmallIcon(R.drawable.settings)
                 .setContentTitle(title)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
