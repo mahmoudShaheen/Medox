860eb76bd17a391f831e4baffe841931e8aceff1
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-20 18:02:22 +0200

    Services triggers
    Services now only start if the user is Senior

--- app/src/main/java/com/slothnull/android/medox/Splash.java
+++ app/src/main/java/com/slothnull/android/medox/Splash.java
@@ -1,12 +1,16 @@
 package com.slothnull.android.medox;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.slothnull.android.medox.service.IndicatorsService;
+import com.slothnull.android.medox.service.LocationService;
 
 public class Splash extends Activity {
 
@@ -43,11 +47,52 @@ public class Splash extends Activity {
     private void callCare(){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
+        stopServices();
+        disableServices();
         finish();
     }
     private void callSenior(){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
+        enableServices();
+        triggerServices();
         finish();
+    }
+    public void triggerServices(){
+        Intent location = new Intent(this, LocationService.class);
+        startService(location);
+        Intent indicators = new Intent(this, IndicatorsService.class);
+        startService(indicators);
+    }
+
+    private void stopServices(){
+        stopService(new Intent(this, LocationService.class));
+        stopService(new Intent(this, IndicatorsService.class));
+    }
+
+    private void enableServices(){
+        Context context = getApplicationContext();
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(
+                new ComponentName(context, LocationService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(
+                new ComponentName(context, IndicatorsService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    private void disableServices(){
+        Context context = getApplicationContext();
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(
+                new ComponentName(context, LocationService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(
+                new ComponentName(context, IndicatorsService.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
     }
 }
