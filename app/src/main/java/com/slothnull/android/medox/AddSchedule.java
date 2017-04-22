6d917ea2e6b4e9adaf07c11541bb2002fef7c758
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-22 21:27:56 +0200

    drug names added to schedule

--- app/src/main/java/com/slothnull/android/medox/AddSchedule.java
+++ app/src/main/java/com/slothnull/android/medox/AddSchedule.java
@@ -2,21 +2,38 @@ package com.slothnull.android.medox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.slothnull.android.medox.Abstract.AbstractSchedule;
+import com.slothnull.android.medox.Abstract.AbstractWarehouse;
 
 public class AddSchedule extends AppCompatActivity {
+
+    final private String TAG = "AddSchedule";
+
+    private TextView drug1;
+    private TextView drug2;
+    private TextView drug3;
+    private TextView drug4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
+        getNames();
+        drug1 = (TextView) findViewById(R.id.drug1View);
+        drug2 = (TextView) findViewById(R.id.drug2View);
+        drug3 = (TextView) findViewById(R.id.drug3View);
+        drug4 = (TextView) findViewById(R.id.drug4View);
     }
 
     public void addEntry(View view){
@@ -52,10 +69,55 @@ public class AddSchedule extends AppCompatActivity {
         mDatabase.setValue(timetable);
         finish();
     }
+
     private String getBills(String bills){
         if (bills.isEmpty()){
             bills = "0";
         }
         return bills;
+    }
+
+    public void getNames(){
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener warehouseListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                for (DataSnapshot child: dataSnapshot.getChildren()) {
+                    AbstractWarehouse warehouse = child.getValue(AbstractWarehouse.class);
+                    if (warehouse.id != null) {
+                        switch (warehouse.id) {
+                            case "1":
+                                drug1.setText(warehouse.name);
+                                break;
+                            case "2":
+                                drug2.setText(warehouse.name);
+                                break;
+                            case "3":
+                                drug3.setText(warehouse.name);
+                                break;
+                            case "4":
+                                drug4.setText(warehouse.name);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        mDatabase.child("users").child(UID).child("warehouse")
+                .addValueEventListener(warehouseListener);
+        //add to list here
+
     }
 }
