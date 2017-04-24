569f42030fa627bdca6858ead39dc07848e21f00
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-24 15:59:19 +0200

    Settings Activity Done :clap:

--- app/src/main/java/com/slothnull/android/medox/Settings.java
+++ app/src/main/java/com/slothnull/android/medox/Settings.java
@@ -1,5 +1,6 @@
 package com.slothnull.android.medox;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
@@ -14,13 +15,18 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.slothnull.android.medox.Abstract.AbstractConfig;
 
 public class Settings extends AppCompatActivity implements LocationListener {
 
     private final String TAG = "SettingsActivity";
+
+    private ProgressDialog mProgressDialog;
 
     private CheckedTextView settingsEnable;
     private CheckedTextView warehouseEnable;
@@ -38,12 +44,17 @@ public class Settings extends AppCompatActivity implements LocationListener {
     private EditText seniorSkype;
     private EditText maxDistance;
 
+    AbstractConfig oldConfig;
+
     public LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        //wait for getting old config
+        showProgressDialog();
+
         checkListeners();
         checkArray = new String[3];
         checkArray[0] = "0";
@@ -55,6 +66,8 @@ public class Settings extends AppCompatActivity implements LocationListener {
         careSkype = (EditText) findViewById(R.id.careSkype);
         seniorSkype = (EditText) findViewById(R.id.seniorSkype);
         maxDistance = (EditText) findViewById(R.id.maxDistance);
+
+        setData();
 
         try{
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -123,19 +136,40 @@ public class Settings extends AppCompatActivity implements LocationListener {
     }
 
     public void save(View v){
-        Log.i(TAG, "Save Called");
+        //strings to send to config class
+        String mMaxDistance = maxDistance.getText().toString();
+        String mMaxHeart = maxHeart.getText().toString();
+        String mMinHeart = minHeart.getText().toString();
+        String mMobileNumber = mobileNumber.getText().toString();
+        String mCareSkype = careSkype.getText().toString();
+        String mSeniorSkype = seniorSkype.getText().toString();
+
+        //if fields are empty save old config
+        if (mMaxDistance.isEmpty())
+            mMaxDistance = oldConfig.maxDistance;
+        if (mMaxHeart.isEmpty())
+            mMaxHeart = oldConfig.maxHeartRate;
+        if (mMinHeart.isEmpty())
+            mMinHeart = oldConfig.minHeartRate;
+        if (mMobileNumber.isEmpty())
+            mMobileNumber = oldConfig.mobileNumber;
+        if (mCareSkype.isEmpty())
+            mCareSkype = oldConfig.careSkype;
+        if (mSeniorSkype.isEmpty())
+            mSeniorSkype = oldConfig.seniorSkype;
+
+
         //send notification to database to access it later in Notification Activity
         String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
         AbstractConfig config = new AbstractConfig(
-                maxDistance.getText().toString(),
+                mMaxDistance,
                 latitude,
                 longitude,
-                maxHeart.getText().toString(),
-                minHeart.getText().toString(),
-                mobileNumber.getText().toString(),
-                careSkype.getText().toString(),
-                seniorSkype.getText().toString(),
+                mMaxHeart,
+                mMinHeart,
+                mMobileNumber,
+                mCareSkype,
+                mSeniorSkype,
                 (checkArray[0] + "," + checkArray[1] +"," + checkArray[2]));
 
 
@@ -162,5 +196,65 @@ public class Settings extends AppCompatActivity implements LocationListener {
     public void onDestroy() {
         super.onDestroy();
         locationManager.removeUpdates(this);
+    }
+
+    public void setData(){
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        ValueEventListener configListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                oldConfig = dataSnapshot.getValue(AbstractConfig.class);
+                if(oldConfig.maxHeartRate != null)
+                    maxHeart.setHint(maxHeart.getHint() + oldConfig.maxHeartRate);
+                if(oldConfig.minHeartRate != null)
+                    minHeart.setHint(minHeart.getHint() + oldConfig.minHeartRate);
+                if(oldConfig.maxDistance != null)
+                    maxDistance.setHint(maxDistance.getHint() + oldConfig.maxDistance);
+                if(oldConfig.homeLatitude != null)
+                    latitude = oldConfig.homeLatitude;
+                if(oldConfig.homeLongitude != null)
+                    longitude = oldConfig.homeLongitude;
+                if(oldConfig.mobileNumber != null)
+                    mobileNumber.setHint(mobileNumber.getHint() + oldConfig.mobileNumber);
+                if(oldConfig.careSkype != null)
+                    careSkype.setHint(careSkype.getHint() + oldConfig.careSkype);
+                if(oldConfig.seniorSkype != null)
+                    seniorSkype.setHint(seniorSkype.getHint() + oldConfig.seniorSkype);
+                if(oldConfig.enabled != null){
+                    checkArray = oldConfig.enabled.split(",");
+                    settingsEnable.setChecked(checkArray[0].equals("1"));
+                    warehouseEnable.setChecked(checkArray[1].equals("1"));
+                    scheduleEnable.setChecked(checkArray[2].equals("1"));
+                }
+                hideProgressDialog();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        mDatabase.child("users").child(UID).child("config")
+                .addValueEventListener(configListener);
+    }
+    //progress dialog to wait for saved data
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setMessage("Loading...");
+        }
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 }
