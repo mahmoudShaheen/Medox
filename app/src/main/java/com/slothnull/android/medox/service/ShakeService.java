46e4b67c12b5a2ab67cd78f6092168561e92e79b
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-25 16:37:23 +0200

    shake service initial code

--- app/src/main/java/com/slothnull/android/medox/service/ShakeService.java
+++ app/src/main/java/com/slothnull/android/medox/service/ShakeService.java
@@ -1,16 +1,46 @@
 package com.slothnull.android.medox.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
-public class ShakeService extends Service {
-    public ShakeService() {
+import com.slothnull.android.medox.fragment.SeniorEmergencyFragment;
+
+public class ShakeService extends Service  {
+
+    private static final String TAG = "ShakeService";
+    public static final String BROADCAST_ACTION = "Hello World";
+
+    Intent intent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "started");
+        intent = new Intent(BROADCAST_ACTION);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    public void sendEmergency(){
+        String title = "Emergency Shake From Watch!";
+        String message = "Action Required IMMEDIATELY !!!!";
+        Context c = getApplicationContext();
+        SeniorEmergencyFragment.emergencyNotification(c, title, message);
     }
 }
