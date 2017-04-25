61f43cb235ef5a451e88db2d8901f448b19ec1ba
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-25 23:28:17 +0200

    toast for settings

--- app/src/main/java/com/slothnull/android/medox/SeniorHome.java
+++ app/src/main/java/com/slothnull/android/medox/SeniorHome.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -155,6 +156,9 @@ public class SeniorHome extends AppCompatActivity {
         if(checkArray[0].equals("1")) {//check if Settings enabled for Senior
             Intent intent = new Intent(this, Settings.class);
             startActivity(intent);
+        }else{
+            Toast.makeText(this, "Settings Access is denied by Care Giver!",
+                    Toast.LENGTH_LONG).show();
         }
     }
 
