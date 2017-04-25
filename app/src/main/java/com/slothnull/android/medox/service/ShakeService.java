aec2fa8e32ce0cb8bc29703ed5f1d98a0ce51afa
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-04-25 21:22:02 +0200

    Shake service running

--- app/src/main/java/com/slothnull/android/medox/service/ShakeService.java
+++ app/src/main/java/com/slothnull/android/medox/service/ShakeService.java
@@ -3,33 +3,44 @@ package com.slothnull.android.medox.service;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
 
 import com.slothnull.android.medox.fragment.SeniorEmergencyFragment;
 
-public class ShakeService extends Service  {
+public class ShakeService extends Service implements SensorEventListener {
 
     private static final String TAG = "ShakeService";
-    public static final String BROADCAST_ACTION = "Hello World";
-
-    Intent intent;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.v(TAG, "started");
-        intent = new Intent(BROADCAST_ACTION);
-    }
-
-    @Override
-    public void onStart(Intent intent, int startId) {
-
-    }
+    //public static final String BROADCAST_ACTION = "Hello World";
+    //Intent intent;
+    // variables for shake detection
+    private static final float SHAKE_THRESHOLD = 18f; // m/S^2
+    private static final int MIN_TIME_BETWEEN_SHAKES_MILLISECS = 1000;
+    private long mLastShakeTime;
+    private SensorManager mSensorMgr;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return START_STICKY;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "started");
+        // Get a sensor manager to listen for shakes
+        mSensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Listen for shakes
+        Sensor accelerometer = mSensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Log.i(TAG,String.valueOf(accelerometer != null) );
+        if (accelerometer != null) {
+            mSensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+        Log.v(TAG, "started Create");
     }
 
     @Override
@@ -37,10 +48,43 @@ public class ShakeService extends Service  {
         return null;
     }
 
-    public void sendEmergency(){
-        String title = "Emergency Shake From Watch!";
-        String message = "Action Required IMMEDIATELY !!!!";
-        Context c = getApplicationContext();
-        SeniorEmergencyFragment.emergencyNotification(c, title, message);
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            if ((curTime - mLastShakeTime) > MIN_TIME_BETWEEN_SHAKES_MILLISECS) {
+
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                double acceleration = Math.sqrt(Math.pow(x, 2) +
+                        Math.pow(y, 2) +
+                        Math.pow(z, 2)) - SensorManager.GRAVITY_EARTH;
+                //Log.d(TAG, "Acceleration is " + acceleration + "m/s^2");
+
+                if (acceleration > SHAKE_THRESHOLD) {
+                    mLastShakeTime = curTime;
+                    Log.i(TAG, "Calling Emergency");
+                    sendEmergency();
+                }
+            }
+        }
     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Ignore
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        // Stop listening for shakes
+        mSensorMgr.unregisterListener(this);
+    }
+
+    public void sendEmergency(){
+        
+    }
+
 }
