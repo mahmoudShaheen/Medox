164629e28e98630ddb865c9ee19bc8e787a276b3
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-06-24 23:52:49 +0200

    adding 8 switches instead of 2

--- app/src/main/java/com/slothnull/android/medox/model/AbstractSwitch.java
+++ app/src/main/java/com/slothnull/android/medox/model/AbstractSwitch.java
@@ -14,14 +14,29 @@ public class AbstractSwitch {
 
     public String switch1;
     public String switch2;
+    public String switch3;
+    public String switch4;
+    public String switch5;
+    public String switch6;
+    public String switch7;
+    public String switch8;
 
     public AbstractSwitch() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public AbstractSwitch(String switch1, String switch2) {
+    public AbstractSwitch(String switch1, String switch2,
+                          String switch3, String switch4,
+                          String switch5, String switch6,
+                          String switch7, String switch8) {
         this.switch1 = switch1;
         this.switch2 = switch2;
+        this.switch3 = switch3;
+        this.switch4 = switch4;
+        this.switch5 = switch5;
+        this.switch6 = switch6;
+        this.switch7 = switch7;
+        this.switch8 = switch8;
     }
 
 }
