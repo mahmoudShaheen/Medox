0744c7e12447589d504ce40951385bbf77a7188c
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-06-29 19:01:36 +0200

    abstract notification class

--- app/src/main/java/com/slothnull/android/medox/model/AbstractNotification.java
+++ app/src/main/java/com/slothnull/android/medox/model/AbstractNotification.java
@@ -16,16 +16,18 @@ public class AbstractNotification {
     public String message;
     public String time;
     public String title;
+    public String to;
 
     public AbstractNotification() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public AbstractNotification(String level, String title, String message, String time) {
+    public AbstractNotification(String level, String title, String message, String time, String to) {
         this.level = level;
         this.message = message;
         this.time = time;
         this.title = title;
+        this.to = to;
     }
 
 }
