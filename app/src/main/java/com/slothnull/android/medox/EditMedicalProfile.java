a00d2d0a3e81d90a01c11648435eba94776c046f
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-07-01 00:21:43 +0200

    medical profile initial activities

---
+++ app/src/main/java/com/slothnull/android/medox/EditMedicalProfile.java
@@ -0,0 +1,13 @@
+package com.slothnull.android.medox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditMedicalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_medical_profile);
+    }
+}
