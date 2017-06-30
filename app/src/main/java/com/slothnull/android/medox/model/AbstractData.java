887899a2b106146168a2a6b7e3031fe712ab2017
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-06-30 08:21:30 +0200

    adding calories to indicators fragment

--- app/src/main/java/com/slothnull/android/medox/model/AbstractData.java
+++ app/src/main/java/com/slothnull/android/medox/model/AbstractData.java
@@ -15,6 +15,7 @@ public class AbstractData {
 
     public String heartRate;
     public String pedo;
+    public String calories;
     public String longitude;
     public String latitude;
     public String billCount;
@@ -23,9 +24,10 @@ public class AbstractData {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public AbstractData(String heartRate, String pedo, String longitude, String latitude, String billCount) {
+    public AbstractData(String heartRate, String pedo, String longitude, String latitude, String billCount, String calories) {
         this.heartRate = heartRate;
         this.pedo = pedo;
+        this.calories = calories;
         this.longitude = longitude;
         this.latitude = latitude;
         this.billCount = billCount;
