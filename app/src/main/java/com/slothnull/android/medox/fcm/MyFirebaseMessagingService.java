198e8face5f5c4c582f3aa2072de457f1bd094c4
mahmoudShaheen <mahmoud_shaheen@live.com>
2017-07-01 02:58:27 +0200

    special ringtone for emergency

--- app/src/main/java/com/slothnull/android/medox/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/slothnull/android/medox/fcm/MyFirebaseMessagingService.java
@@ -29,6 +29,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -145,7 +146,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, level /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.notification)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(),
@@ -153,11 +153,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setContentTitle(title)
                 .setContentText(messageBody)
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
+
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if(level == 1){ //special ringtone for emergency
+            MediaPlayer mp= MediaPlayer.create(getApplicationContext(), R.raw.emergency);
+            mp.start();
+        }
 
         notificationManager.notify(level /* ID of notification */, notificationBuilder.build());
     }
